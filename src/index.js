6476dde879bf52efd36d82a6f1058a860de60e50
Raman <raman.mastyka@gmail.com>
2019-03-13 22:09:41 +0300

    100%

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
+const specSymbols = ["(",")","|","{","}","[","]"];
+
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const regStr = bracketsConfig.map(([open, close]) => {
+    if (specSymbols.includes(open)) open = "\\" + open;
+    if (specSymbols.includes(close)) close = "\\" + close;
+    return `(${open}${close})`;
+  }).join("|");
+  const re = new RegExp(regStr);
+  do {
+    str = str.replace(re, "");
+  } while (~str.search(re));
+  return !str.length;
+};
